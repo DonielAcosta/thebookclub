dc5aa18481f5c444a2be223334854dc89a338272
Doniel Acosta <donielacosta1995@gmail.com>
2024-11-15 08:36:47 -0400

    mvc and model and relations

--- commands/SoportController.php
+++ commands/SoportController.php
@@ -6,6 +6,7 @@ namespace app\commands;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use app\models\Book;
+use app\models\Author;
 /**
  * Este controlador de consola para pruebas
  *
@@ -42,14 +43,45 @@ class SoportController extends Controller{
             $data =fgetcsv($f);
             // print_r($data);
             if(!empty($data[0]) && !empty($data[2])){
+                $author = Author::find()->where(['name'=>$data[3]])->one();
+                if(empty($author)){
+                    $author = new Author;
+                    $author->name = $data[3];
+                    $author->save();
+                }
                 $book = new Book;
-                $book->title = $data[0];
-                $book->author = $data[2];
-                $book = $this->quick($book);
+                $book->title = $data[1];
+                $book->author_id = $author->id;
+                $book->save();
+                // $book->author = $data[2];
+                // $book = $this->quick($book);
                 printf("%s\n", $book->toString());
             }
         }
         fclose($f);
+        return ExitCode::OK;
+    }
+
+    public function actionAuthor($author_id){
+        $author = Author::findOne($author_id);
+        if(empty($author)){
+            printf("no existe author\n");
+            return ExitCode::DATAERR;
+        }
+        printf("%s \n", $author->toString());
+        foreach ($author->books as $book){
+            printf(" - %s \n", $book->toString());
+        }
+        return ExitCode::OK;
+    }
+
+    public function actionBook($book_id){
+        $book = Book::findOne($book_id);
+        if(empty($book)){
+            printf("no existe book\n");
+            return ExitCode::DATAERR;
+        }
+        printf("%s \n", $book->toString());
         return ExitCode::OK;
     }
 
--- models/Book.php
+++ models/Book.php
@@ -2,14 +2,28 @@
 
 namespace app\models;
 
-use yii\base\Model;
-
-class Book extends Model{
-    public $title;
-    public $author;
-
+use yii\db\ActiveRecord;
+class Book extends ActiveRecord{
+    public static function tableName(){
+        return "books";
+    }
+    public function getId(){
+        return $this->books_id;
+    }
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function getAuthor(){
+        return $this->hasOne(Author::class, ['author_id' => 'author_id'])->one();
+    }
     public function toString(){
-        return sprintf("%s - %s", $this->title, $this->author);
+        return sprintf("(%d) %s - %s",
+         $this->id,
+         $this->title,
+         $this->getAuthor()->name
+        );
     }
 
 }
